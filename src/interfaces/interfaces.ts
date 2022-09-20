236d90c14e71af0a80913060070124cb124f9037
404decades <pipech1616@gmail.com>
2022-09-20 17:53:38 -0500

    version 1.0.3

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -57,16 +57,16 @@ export interface ObjectTypes {
     beard: { [key: string]: BeardTypes };
 }
 
-export type EyesTypes = 'eyes-1' | 'eyes-2' ;
-export type EyesAccessoryTypes = 'eyes-accessory-1' | 'eyes-accessory-2' | 'eyes-accessory-3' | 'eyes-accessory-4' | 'eyes-accessory-5'| 'eyes-accessory-6'| 'eyes-accessory-7'| 'eyes-accessory-8'| 'eyes-accessory-9' | 'eyes-accessory-10' | 'eyes-accessory-11';
-export type EarAccessoryTypes = 'ear-accessory-1' | 'ear-accessory-2' | 'ear-accessory-3'  | 'ear-accessory-4'  | 'ear-accessory-5' ;
-export type HairTypes = 'hair-1' | 'hair-2'| 'hair-3'| 'hair-4'| 'hair-5' | 'hair-6'| 'hair-7' | 'hair-8'| 'hair-9'| 'hair-10'| 'hair-11' | 'hair-12'| 'hair-13'| 'hair-14'| 'hair-15'| 'hair-16'| 'hair-17' | 'hair-18' | 'hair-19' | 'hair-20' | 'hair-21' | 'hair-22' | 'hair-23' | 'hair-24' | 'hair-25' | 'hair-26' | 'hair-27' | 'hair-28' | 'hair-29' | 'hair-30' | 'hair-31';
-export type HeadAccessoryTypes = 'head-accessory-1' | 'head-accessory-2'| 'head-accessory-3' | 'head-accessory-4'  | 'head-accessory-5' | 'head-accessory-6' | 'head-accessory-7' | 'head-accessory-8' | 'head-accessory-9' | 'head-accessory-10' | 'head-accessory-11' | 'head-accessory-12' | 'head-accessory-13' | 'head-accessory-14' | 'head-accessory-15' | 'head-accessory-16';
-export type NoseTypes = 'nose-1' | 'nose-2'  | 'nose-3';
-export type MouthTypes = 'mouth-1' | 'mouth-2'  | 'mouth-3' | 'mouth-4' | 'mouth-5';
-export type MouthAccessoryTypes = 'mouth-accessory-1' | 'mouth-accessory-2'| 'mouth-accessory-3' | 'mouth-accessory-4'  ;
-export type NeckAccessoryTypes = 'neck-accessory-1' | 'neck-accessory-2' | 'neck-accessory-3' | 'neck-accessory-4' | 'neck-accessory-5';
-export type BeardTypes = 'beard-1' | 'beard-2' | 'beard-3' | 'beard-4' | 'beard-5'  | 'beard-6' | 'beard-7' | 'beard-8' | 'beard-9' | 'beard-10'  | 'beard-11'  ;
+export type EyesTypes = string  | 'eyes-1' | 'eyes-2' ;
+export type EyesAccessoryTypes =  string  | 'eyes-accessory-1' | 'eyes-accessory-2' | 'eyes-accessory-3' | 'eyes-accessory-4' | 'eyes-accessory-5'| 'eyes-accessory-6'| 'eyes-accessory-7'| 'eyes-accessory-8'| 'eyes-accessory-9' | 'eyes-accessory-10' | 'eyes-accessory-11';
+export type EarAccessoryTypes =  string  | 'ear-accessory-1' | 'ear-accessory-2' | 'ear-accessory-3'  | 'ear-accessory-4'  | 'ear-accessory-5' ;
+export type HairTypes =  string  | 'hair-1' | 'hair-2'| 'hair-3'| 'hair-4'| 'hair-5' | 'hair-6'| 'hair-7' | 'hair-8'| 'hair-9'| 'hair-10'| 'hair-11' | 'hair-12'| 'hair-13'| 'hair-14'| 'hair-15'| 'hair-16'| 'hair-17' | 'hair-18' | 'hair-19' | 'hair-20' | 'hair-21' | 'hair-22' | 'hair-23' | 'hair-24' | 'hair-25' | 'hair-26' | 'hair-27' | 'hair-28' | 'hair-29' | 'hair-30' | 'hair-31';
+export type HeadAccessoryTypes =  string  | 'head-accessory-1' | 'head-accessory-2'| 'head-accessory-3' | 'head-accessory-4'  | 'head-accessory-5' | 'head-accessory-6' | 'head-accessory-7' | 'head-accessory-8' | 'head-accessory-9' | 'head-accessory-10' | 'head-accessory-11' | 'head-accessory-12' | 'head-accessory-13' | 'head-accessory-14' | 'head-accessory-15' | 'head-accessory-16';
+export type NoseTypes =  string  | 'nose-1' | 'nose-2'  | 'nose-3';
+export type MouthTypes =  string  | 'mouth-1' | 'mouth-2'  | 'mouth-3' | 'mouth-4' | 'mouth-5';
+export type MouthAccessoryTypes =  string  | 'mouth-accessory-1' | 'mouth-accessory-2'| 'mouth-accessory-3' | 'mouth-accessory-4'  ;
+export type NeckAccessoryTypes =  string  | 'neck-accessory-1' | 'neck-accessory-2' | 'neck-accessory-3' | 'neck-accessory-4' | 'neck-accessory-5';
+export type BeardTypes =  string  | 'beard-1' | 'beard-2' | 'beard-3' | 'beard-4' | 'beard-5'  | 'beard-6' | 'beard-7' | 'beard-8' | 'beard-9' | 'beard-10'  | 'beard-11'  ;
 
 
 export interface DataItem {
